82cc6ebaee352e184d56d3cdd2a931c62cf30f71
ibukun-koyer <ji81778@umbc.edu>
2021-09-24 11:31:15 -0400

    changed resume

--- downloadResume.js
+++ downloadResume.js
@@ -2,7 +2,7 @@ const resumeBtn = document.querySelector(".resumeDownload");
 
 resumeBtn.addEventListener("click", () => {
   var anchor = document.createElement("a");
-  anchor.href = "./afolabi's resume.pdf";
+  anchor.href = "./september resume one line extra curriculum.pdf";
   anchor.target = "_blank";
   anchor.download = "Ibukun's resume";
   anchor.click();
