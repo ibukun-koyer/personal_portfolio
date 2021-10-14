6f9170c8a08a34f4acebd6f8c9132615bf923a97
ibukun-koyer <ji81778@umbc.edu>
2021-10-14 09:41:45 -0400

    fixed some styles

--- time_of_day_bkg.js
+++ time_of_day_bkg.js
@@ -45,7 +45,7 @@ function change_bkg() {
   } else {
     parsed_time = parsed_time === 12 ? (parsed_time += 12) : parsed_time;
   }
-
+  parsed_time = 18;
   function setDesign(i) {
     if (i !== prevI) {
       //get the bkg
